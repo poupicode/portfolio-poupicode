05c872de3e68dee9d21baa168bd64dbfdb084cc7
poupicode <gillesmarchesse@gmail.com>
2026-02-07 17:11:52 +0100

    test cicd

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-6xl font-bold mb-4">
-          Portfolio Test
+          Portfolio Test - cicd aussi???
         </h1>
         <p className="text-xl text-muted">
           Le setup fonctionne ! ✅
